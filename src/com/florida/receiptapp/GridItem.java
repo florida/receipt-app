8542f746afda1fbf0252ad697dce54c79c69e4eb
Florida Elago <florida.elago@gmail.com>
2013-11-30 14:39:47 -0600

    add gridlayout on home

---
+++ src/com/florida/receiptapp/GridItem.java
@@ -0,0 +1,30 @@
+package com.florida.receiptapp;
+
+import android.graphics.Bitmap;
+
+public class GridItem {
+	Bitmap image;
+	String title;
+	
+	public GridItem(Bitmap image, String title) {
+		super();
+		this.image = image;
+		this.title = title;
+	}
+	
+	public Bitmap getImage() {
+		return image;
+	}
+	
+	public void setImage(Bitmap image) {
+		this.image = image;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+}
---
+++ src/com/florida/receiptapp/HomeNavGridAdapter.java
@@ -0,0 +1,57 @@
+package com.florida.receiptapp;
+
+import java.util.ArrayList;
+
+import com.florida.receiptapp.R;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class HomeNavGridAdapter extends ArrayAdapter<GridItem> {
+	Context context;
+	int layout_resource_id;
+	ArrayList<GridItem> data = new ArrayList<GridItem>();
+	
+	public HomeNavGridAdapter(Context context, int layout_resource_id, ArrayList<GridItem> data) {
+		super(context, layout_resource_id, data);
+		this.context = context;
+		this.layout_resource_id = layout_resource_id;
+		this.data = data;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View row = convertView;
+		RowHolder holder = null;
+		
+		if (row == null) {
+			LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+			row = inflater.inflate(layout_resource_id, parent, false);
+			
+			holder = new RowHolder();
+			holder.txt_title = (TextView) row.findViewById(R.id.item_text);
+			holder.image_item = (ImageView) row.findViewById(R.id.item_image);
+			row.setTag(holder);
+		} else {
+			holder = (RowHolder) row.getTag();
+		}
+		
+		GridItem item = data.get(position);
+		holder.txt_title.setText(item.getTitle());
+		holder.image_item.setImageBitmap(item.getImage());
+		return row;
+	}
+	
+	static class RowHolder {
+		TextView txt_title;
+		ImageView image_item;
+	}
+	
+	
+}
