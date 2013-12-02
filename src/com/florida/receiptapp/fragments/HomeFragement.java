4165544c741dc76452cf6fee9547c5bf82cce942
Florida Elago <florida.elago@gmail.com>
2013-12-01 23:22:57 -0600

    add new callbacks to listener

--- src/com/florida/receiptapp/fragments/HomeFragement.java
+++ src/com/florida/receiptapp/fragments/HomeFragement.java
@@ -2,19 +2,7 @@ package com.florida.receiptapp.fragments;
 
 import java.util.ArrayList;
 
-import com.florida.receiptapp.AddReceiptActivity;
-import com.florida.receiptapp.MainActivity;
-import com.florida.receiptapp.R;
-import com.florida.receiptapp.R.drawable;
-import com.florida.receiptapp.R.id;
-import com.florida.receiptapp.R.layout;
-import com.florida.receiptapp.adapters.HomeNavGridAdapter;
-import com.florida.receiptapp.classes.GridItem;
-
-import android.app.FragmentTransaction;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -24,6 +12,14 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 
+import com.florida.receiptapp.AboutAppDialog;
+import com.florida.receiptapp.AddCategoryDialogActivity;
+import com.florida.receiptapp.AddReceiptActivity;
+import com.florida.receiptapp.MainActivity;
+import com.florida.receiptapp.R;
+import com.florida.receiptapp.adapters.HomeNavGridAdapter;
+import com.florida.receiptapp.classes.GridItem;
+
 public class HomeFragement extends Fragment {
 	GridView grid_view;
 	ArrayList<GridItem> grid_array;
@@ -31,22 +27,17 @@ public class HomeFragement extends Fragment {
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.home_fragment, container, false);
-		
-		Bitmap camera = BitmapFactory.decodeResource(this.getResources(), R.drawable.ic_action_camera);
-		Bitmap receipts = BitmapFactory.decodeResource(this.getResources(), R.drawable.receipts);
-		Bitmap receipt_add = BitmapFactory.decodeResource(this.getResources(), R.drawable.receipt_add);
-		// Bitmap receipts = BitmapFactory.decodeResource(this.getResources(), R.drawable.receipts);
+		View view = inflater.inflate(R.layout.fragment_home, container, false);
 		// Bitmap receipt_add = BitmapFactory.decodeResource(this.getResources(), R.drawable.receipt_add);
 		
 		grid_array = new ArrayList<GridItem>();
-		grid_array.add(new GridItem(receipt_add, "Add Receipt"));
-		grid_array.add(new GridItem(receipts, " Receipts"));
-		grid_array.add(new GridItem(receipt_add, "Add Category"));
-		grid_array.add(new GridItem(receipts, "Categories"));
-		grid_array.add(new GridItem(receipt_add, "Settings"));
-		grid_array.add(new GridItem(receipt_add, "About App"));
-		grid_array.add(new GridItem(receipts, " Quit"));
+		grid_array.add(new GridItem("ic_receipt_add", "Add Receipt", getActivity()));
+		grid_array.add(new GridItem("ic_receipts", " Receipts", getActivity()));
+		grid_array.add(new GridItem("ic_category_add", "Add Category", getActivity()));
+		grid_array.add(new GridItem("ic_categories", "Categories", getActivity()));
+		grid_array.add(new GridItem("ic_settings", "Settings", getActivity()));
+		grid_array.add(new GridItem("ic_about", "About App", getActivity()));
+		grid_array.add(new GridItem("ic_quit", " Quit", getActivity()));
 		
 		grid_view = (GridView) view.findViewById(R.id.gridView1);
 		grid_adapter = new HomeNavGridAdapter(getActivity(), R.layout.home_row_grid, grid_array);
@@ -58,24 +49,42 @@ public class HomeFragement extends Fragment {
 			public void onItemClick(AdapterView<?> parent, View view, int position,
 					long id) {
 				switch (position) {
-				case 0:
-					 //getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new CameraFragment()).addToBackStack(null).commit();
-					break;
-				case 1:
+				case 0: 
 					Intent intent = new Intent(getActivity(), AddReceiptActivity.class);
 					startActivity(intent);
 					break;
-				case 2:
+				case 1:
 					ReceiptListFragment receipt_list_fragment = null;
 					if (MainActivity.receipt_list_fragment == null) {
 						receipt_list_fragment = new ReceiptListFragment();
 						MainActivity.receipt_list_fragment = receipt_list_fragment;
-					} else {
-						receipt_list_fragment = MainActivity.receipt_list_fragment;
-					}
-					getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, receipt_list_fragment).addToBackStack(null).commit();
-					break;	
+					} else { receipt_list_fragment = MainActivity.receipt_list_fragment; }
+					
+					getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, receipt_list_fragment).addToBackStack(null).commit();
+					break;
+				case 2:
+					Intent i = new Intent(getActivity(), AddCategoryDialogActivity.class);
+		        	startActivity(i);
+					break;
+				case 3:
+					CategoriesFragment categories_fragment = null;
+					if (MainActivity.categories_fragment == null) {
+						categories_fragment = new CategoriesFragment();
+						MainActivity.categories_fragment = categories_fragment;
+					} else { categories_fragment = MainActivity.categories_fragment; } 
+					getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, categories_fragment).addToBackStack(null).commit();
+					break;
+				case 4: 
+					
+					break;
+				case 5:
+					(new AboutAppDialog(getActivity())).show();
+					break;
+				case 6: 
+					System.exit(0);
+					break;
 				}
+			
 			}
 		});
 		return view;
