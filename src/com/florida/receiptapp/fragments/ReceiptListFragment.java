4a3d5ec116d6f58858866fefbdb80ee4ee9b2bba
Florida Elago <florida.elago@gmail.com>
2013-12-01 23:22:22 -0600

    add default select

--- src/com/florida/receiptapp/fragments/ReceiptListFragment.java
+++ src/com/florida/receiptapp/fragments/ReceiptListFragment.java
@@ -1,5 +1,7 @@
 package com.florida.receiptapp.fragments;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,16 +16,16 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import com.florida.receiptapp.AddReceiptActivity;
+import com.florida.receiptapp.CommonFunctions;
 import com.florida.receiptapp.R;
-import com.florida.receiptapp.R.id;
-import com.florida.receiptapp.R.layout;
 import com.florida.receiptapp.adapters.ReceiptListAdapter;
 import com.florida.receiptapp.classes.Category;
 import com.florida.receiptapp.classes.Receipt;
+import com.parse.ParseQuery;
 import com.parse.ParseQueryAdapter;
+import com.parse.ParseQueryAdapter.OnQueryLoadListener;
 
 public class ReceiptListFragment extends Fragment {
 	public static final int REQUEST_CODE_STANDARD = 1;
@@ -32,9 +34,9 @@ public class ReceiptListFragment extends Fragment {
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.receipt_list_view, container, false);
+		View view = inflater.inflate(R.layout.fragment_receipts, container, false);
 		
-		final ListView listview = (ListView) view.findViewById(R.id.receipt_listview);
+		final ListView listview = (ListView) view.findViewById(R.id.listview_receipt);
 		
 		Button btn_add_receipt = (Button) view.findViewById(R.id.btn_add_receipt);
 		btn_add_receipt.setOnClickListener(new OnClickListener() {
@@ -46,13 +48,19 @@ public class ReceiptListFragment extends Fragment {
 			}
 		});
 		
-		category_adapter = new ParseQueryAdapter<Category>(getActivity(), Category.class);
+		category_adapter = new ParseQueryAdapter<Category>(getActivity(), new ParseQueryAdapter.QueryFactory<Category>() {
+			public ParseQuery<Category> create() {
+				ParseQuery<Category> query = ParseQuery.getQuery("Category");
+				query.setCachePolicy(ParseQuery.CachePolicy.NETWORK_ELSE_CACHE);
+				return query;
+			}
+		});
 		category_adapter.setTextKey("name");
 		category_adapter.setPaginationEnabled(false);
-		Spinner spn_category = (Spinner) view.findViewById(R.id.receipt_categories);
+		final Spinner spn_category = (Spinner) view.findViewById(R.id.spn_receipt_categories);
 		
 		spn_category.setOnItemSelectedListener(new OnItemSelectedListener() {
-
+			
 			@Override
 			public void onItemSelected(AdapterView<?> parent, View view,
 					int position, long id) {
@@ -70,17 +78,28 @@ public class ReceiptListFragment extends Fragment {
 			}
 
 			@Override
-			public void onNothingSelected(AdapterView<?> arg0) {
-				// TODO Auto-generated method stub
-				
+			public void onNothingSelected(AdapterView<?> arg0) {			
 			}
 			
 		});
+		listview.setAdapter(adapter);
 		spn_category.setAdapter(category_adapter);
+		
+		category_adapter.addOnQueryLoadListener(new OnQueryLoadListener<Category>() {
+
+			@Override
+			public void onLoaded(List<Category> arg0, Exception arg1) {
+					spn_category.setSelection(CommonFunctions.getIndex(spn_category, "H9MEsdbCZT"));
+			}
+
+			@Override
+			public void onLoading() {
+			}
+		 });
 		
 		adapter = new ReceiptListAdapter(getActivity());
 		
-		listview.setAdapter(adapter);
+		
 		listview.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
