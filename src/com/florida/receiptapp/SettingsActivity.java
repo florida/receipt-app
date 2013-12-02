64f922e8fa22bb28d9fcebb432f5a3006c7e5834
Florida Elago <florida.elago@gmail.com>
2013-12-01 23:49:52 -0600

    add settings activity

---
+++ src/com/florida/receiptapp/SettingsActivity.java
@@ -0,0 +1,27 @@
+package com.florida.receiptapp;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SettingsActivity extends PreferenceActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+	}
+	
+	public static class MyPreferenceFragment extends PreferenceFragment {
+
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.settings);
+		}
+		
+	}
+
+}
+
