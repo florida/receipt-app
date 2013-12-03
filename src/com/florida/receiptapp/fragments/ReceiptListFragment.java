58dfb6f28e87cd0c680ce4118866ec02aec97f2a
Florida Elago <florida.elago@gmail.com>
2013-12-03 01:38:06 -0600

    add prefs and adjust to show receipts for users only

--- src/com/florida/receiptapp/fragments/ReceiptListFragment.java
+++ src/com/florida/receiptapp/fragments/ReceiptListFragment.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,19 +28,22 @@ import com.florida.receiptapp.classes.Category;
 import com.florida.receiptapp.classes.Receipt;
 import com.parse.ParseQuery;
 import com.parse.ParseQueryAdapter;
+import com.parse.ParseUser;
 import com.parse.ParseQueryAdapter.OnQueryLoadListener;
 
 public class ReceiptListFragment extends Fragment {
 	public static final int REQUEST_CODE_STANDARD = 1;
 	ParseQueryAdapter<Category> category_adapter;
 	ReceiptListAdapter adapter;
+	ListView lv;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		
 		View view = inflater.inflate(R.layout.fragment_receipts, container, false);
 		
 		final ListView listview = (ListView) view.findViewById(R.id.listview_receipt);
-		
+		lv = listview;
 		Button btn_add_receipt = (Button) view.findViewById(R.id.btn_add_receipt);
 		btn_add_receipt.setOnClickListener(new OnClickListener() {
 			
@@ -52,6 +58,7 @@ public class ReceiptListFragment extends Fragment {
 			public ParseQuery<Category> create() {
 				ParseQuery<Category> query = ParseQuery.getQuery("Category");
 				query.setCachePolicy(ParseQuery.CachePolicy.NETWORK_ELSE_CACHE);
+				query.whereEqualTo("user", ParseUser.getCurrentUser());
 				return query;
 			}
 		});
@@ -64,10 +71,10 @@ public class ReceiptListFragment extends Fragment {
 			@Override
 			public void onItemSelected(AdapterView<?> parent, View view,
 					int position, long id) {
+				String category_name = ((Category) parent.getItemAtPosition(position)).getName();
 				String category_id = ((Category) parent.getItemAtPosition(position)).getObjectId();
 				
-				
-				if (category_id.equals("H9MEsdbCZT")) {
+				if (category_name.equals("All")) {
 					adapter = new ReceiptListAdapter(getActivity());
 				} else {
 					adapter = new ReceiptListAdapter(getActivity(), category_id);
@@ -75,6 +82,7 @@ public class ReceiptListFragment extends Fragment {
 				
 				listview.setAdapter(adapter);
 				adapter.loadObjects();
+				
 			}
 
 			@Override
@@ -89,7 +97,9 @@ public class ReceiptListFragment extends Fragment {
 
 			@Override
 			public void onLoaded(List<Category> arg0, Exception arg1) {
-					spn_category.setSelection(CommonFunctions.getIndex(spn_category, "H9MEsdbCZT"));
+					SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getActivity());
+					String default_settings = settings.getString((String) getResources().getText(R.string.pref_key_receipt_list_default_sort), "all");
+					spn_category.setSelection(CommonFunctions.getIndex(spn_category, default_settings));
 			}
 
 			@Override
@@ -116,9 +126,26 @@ public class ReceiptListFragment extends Fragment {
 			}
 		});
 		
+		SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getActivity());
+		listview.setBackgroundColor(settings.getInt((String) getResources().getText(R.string.pref_key_receipts_background_color), Color.TRANSPARENT));
 		return view;
 	}
 	
+	
+	
+	@Override
+	public void onResume() {
+		// TODO Auto-generated method stub
+		SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getActivity());
+		lv.setBackgroundColor(settings.getInt((String) getResources().getText(R.string.pref_key_receipts_background_color), Color.TRANSPARENT));
+		adapter.loadObjects();
+		category_adapter.loadObjects();
+		super.onResume();
+		
+	}
+
+
+
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		// TODO Auto-generated method stub
