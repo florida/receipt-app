0de7b6e2a6d4de74d946c963faf06a99d2271dd8
Florida Elago <florida.elago@gmail.com>
2013-12-02 21:46:36 -0600

    rename views

--- src/com/florida/receiptapp/AddCategoryDialogActivity.java
+++ src/com/florida/receiptapp/AddCategoryDialogActivity.java
@@ -6,7 +6,6 @@ import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -34,8 +33,8 @@ public class AddCategoryDialogActivity extends Activity {
 		
 		category_name = ((EditText) findViewById(R.id.edtxt_category_name));
 		
-		create = (Button) findViewById(R.id.create_category);
-		cancel = (Button) findViewById(R.id.cancel_create_category);
+		create = (Button) findViewById(R.id.btn_create_category);
+		cancel = (Button) findViewById(R.id.btn_cancel_create_category);
 		
 		ButtonListener listener = new ButtonListener();
 		create.setOnClickListener(listener);
@@ -57,45 +56,47 @@ public class AddCategoryDialogActivity extends Activity {
 		}
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.add_category_dialog, menu);
-		return true;
-	}
-
 	private class ButtonListener implements android.view.View.OnClickListener {
 
 		@Override
 		public void onClick(View v) {
 			switch (v.getId()) {
-			case R.id.create_category:
-				if (category == null) {
-					category = new Category();
+			case R.id.btn_create_category:
+				
+				ParseQuery<Category> query = ParseQuery.getQuery("Category");
+				query.setCachePolicy(ParseQuery.CachePolicy.NETWORK_ELSE_CACHE);
+				query.whereEqualTo("name", category_name.getText().toString());
+				Category unique_result = null;
+				try {
+					unique_result = query.getFirst();
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-				category.setName(category_name.getText().toString());
-				category.saveInBackground();
-				setResult(Activity.RESULT_OK);
-				finish();
-				break;
-			case R.id.cancel_create_category:
-				if (category != null) {
-					AlertDialog.Builder alert_dialog = new AlertDialog.Builder(AddCategoryDialogActivity.this);
-					alert_dialog.setTitle("Confirm Delete...");
-					alert_dialog.setMessage("Are you sure you want to delete this category?");
-					alert_dialog.setIcon(R.drawable.ic_action_navigation_cancel);
-					alert_dialog.setPositiveButton("Yes", new OnClickListener() {
-						
-						@Override
-						public void onClick(DialogInterface dialog, int which) {
-							Toast.makeText(AddCategoryDialogActivity.this, "Category deleted", Toast.LENGTH_LONG).show();
-							category.deleteInBackground();
-							setResult(Activity.RESULT_OK);
-							finish();
-						}
-					});
+				
+				if (unique_result == null || (category != null && category.getName().equals(category_name.getText().toString()))) {
+					String message = (category  != null) ? "Category Saved" : "Category Created";
+
+					category = (category == null) ? new Category() : category;
 					
-					alert_dialog.setNegativeButton("No", new OnClickListener() {
+					
+					Toast.makeText(AddCategoryDialogActivity.this, message, Toast.LENGTH_LONG).show();
+					
+					category.setName(category_name.getText().toString());
+					if (CommonFunctions.CheckNetworkStatus(AddCategoryDialogActivity.this)) {
+						category.saveInBackground();
+					} else {
+						category.saveEventually();
+					}
+					
+					setResult(Activity.RESULT_OK);
+					finish();
+				} else {
+					AlertDialog.Builder alert_dialog = new AlertDialog.Builder(AddCategoryDialogActivity.this);
+					alert_dialog.setTitle("Category Already Exists...");
+					alert_dialog.setMessage("Can't create duplicate categories");
+					alert_dialog.setIcon(R.drawable.ic_action_navigation_cancel);
+					alert_dialog.setPositiveButton("Ok", new OnClickListener() {
 						
 						@Override
 						public void onClick(DialogInterface dialog, int which) {
@@ -103,6 +104,60 @@ public class AddCategoryDialogActivity extends Activity {
 						}
 					});
 					alert_dialog.show();
+				}
+				
+				
+				break;
+			case R.id.btn_cancel_create_category:
+				if (category != null) {
+					
+					if (category.getObjectId().equals("H9MEsdbCZT") || category.getObjectId().equals("6gvRYQYqFj")) {
+						AlertDialog.Builder alert_dialog = new AlertDialog.Builder(AddCategoryDialogActivity.this);
+						alert_dialog.setTitle("Can't delete category");
+						alert_dialog.setMessage("Can't create default categories");
+						alert_dialog.setIcon(R.drawable.ic_action_navigation_cancel);
+						alert_dialog.setPositiveButton("Ok", new OnClickListener() {
+							
+							@Override
+							public void onClick(DialogInterface dialog, int which) {
+								dialog.cancel();
+							}
+						});
+						alert_dialog.show();
+					} else {
+						AlertDialog.Builder alert_dialog = new AlertDialog.Builder(AddCategoryDialogActivity.this);
+						alert_dialog.setTitle("Confirm Delete...");
+						alert_dialog.setMessage("Are you sure you want to delete this category?");
+						alert_dialog.setIcon(R.drawable.ic_action_navigation_cancel);
+						alert_dialog.setPositiveButton("Yes", new OnClickListener() {
+							
+							@Override
+							public void onClick(DialogInterface dialog, int which) {
+								
+								
+								
+								Toast.makeText(AddCategoryDialogActivity.this, "Category deleted", Toast.LENGTH_LONG).show();
+								if (CommonFunctions.CheckNetworkStatus(AddCategoryDialogActivity.this)) {
+									category.deleteInBackground();
+								} else {
+									category.deleteEventually();
+									
+								}
+								
+								setResult(Activity.RESULT_OK);
+								finish();
+							}
+						});
+						
+						alert_dialog.setNegativeButton("No", new OnClickListener() {
+							
+							@Override
+							public void onClick(DialogInterface dialog, int which) {
+								dialog.cancel();
+							}
+						});
+						alert_dialog.show();
+					}
 					
 				} else {
 					finish();
