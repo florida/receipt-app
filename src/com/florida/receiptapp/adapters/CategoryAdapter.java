e534184f9332cf58bf4f12329664c422c6789f36
Florida Elago <florida.elago@gmail.com>
2013-12-03 01:40:08 -0600

    load only categories by users

--- src/com/florida/receiptapp/adapters/CategoryAdapter.java
+++ src/com/florida/receiptapp/adapters/CategoryAdapter.java
@@ -9,6 +9,7 @@ import com.florida.receiptapp.R;
 import com.florida.receiptapp.classes.Category;
 import com.parse.ParseQuery;
 import com.parse.ParseQueryAdapter;
+import com.parse.ParseUser;
 
 public class CategoryAdapter extends ParseQueryAdapter<Category>{
 	
@@ -19,6 +20,7 @@ public class CategoryAdapter extends ParseQueryAdapter<Category>{
 				// only top-rated meals.
 				ParseQuery<Category> query = ParseQuery.getQuery("Category");
 				query.setCachePolicy(ParseQuery.CachePolicy.NETWORK_ELSE_CACHE);
+				query.whereEqualTo("user", ParseUser.getCurrentUser());
 				return query;
 			}
 		});
