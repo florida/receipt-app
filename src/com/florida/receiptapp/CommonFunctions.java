e71aefeb5c2a135076926ff2117b6d36edc38b1c
Florida Elago <florida.elago@gmail.com>
2013-12-02 21:47:06 -0600

    add new common functions

--- src/com/florida/receiptapp/CommonFunctions.java
+++ src/com/florida/receiptapp/CommonFunctions.java
@@ -1,16 +1,30 @@
 package com.florida.receiptapp;
 
+import com.florida.receiptapp.classes.Category;
+
 import android.app.Activity;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.widget.Spinner;
 
 
 public class CommonFunctions {
 	
-	public boolean CheckNetworkStatus(Context context) {	
+	public static boolean CheckNetworkStatus(Context context) {	
 		ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Activity.CONNECTIVITY_SERVICE);
 		NetworkInfo netInfo = cm.getActiveNetworkInfo();
 		return  (netInfo != null && netInfo.isConnectedOrConnecting());
+	}
+	
+	public static int getIndex(Spinner spinner, String string) {	
+		int index = 0;
+	
+		for (int i = 0; i < spinner.getAdapter().getCount(); i++) {
+			if (((Category) spinner.getItemAtPosition(i)).getObjectId().equals(string)) {
+				index = i;
+			}
+		}
+		return index;
 	}
 }
