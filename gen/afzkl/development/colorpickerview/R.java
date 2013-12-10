9415e2972a9c72e11db6291b74397f6e230501f7
Florida Elago <florida.elago@gmail.com>
2013-12-09 20:57:22 -0600

    new generated ids

--- gen/afzkl/development/colorpickerview/R.java
+++ gen/afzkl/development/colorpickerview/R.java
@@ -16,7 +16,7 @@ public final class R {
 		public static final int showSelectedColorInList = 0x7f010001;
 	}
 	public static final class drawable {
-		public static final int icon = 0x7f020015;
+		public static final int icon = 0x7f02001c;
 	}
 	public static final class id {
 		public static final int color_panel_new = 0x7f050002;
